c2d9097ddcfd929401d7bf76bc52c7ffcbfbbfe2
Andrei Freeman <andrei@lordandrei.com>
2022-07-25 19:04:49 -0400

    Initial Commit

---
+++ dmed/dmedApp.swift
@@ -0,0 +1,17 @@
+//
+//  dmedApp.swift
+//  dmed
+//
+//  Created by Andrei Freeman on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct dmedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
