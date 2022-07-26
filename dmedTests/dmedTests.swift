7558d9700fcc690bc8bf9e8b33f0bbb5e553874e
Andrei Freeman <andrei@lordandrei.com>
2022-07-26 08:54:51 -0400

    Starting Unit Tests.

--- dmedTests/dmedTests.swift
+++ dmedTests/dmedTests.swift
@@ -18,12 +18,19 @@ class dmedTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testMD5() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+      var baseText = "1"
+      var hashText = MD5(string: baseText)
+      XCTAssertEqual(hashText, "c4ca4238a0b923820dcc509a6f75849b")
+
+      baseText = "Hello, World"
+      hashText = MD5(string: baseText)
+      XCTAssertEqual(hashText, "82bb413746aee42f89dea2b59614f9ef")
     }
 
     func testPerformanceExample() throws {
